336b1ed1e27109d81237b88777863906603d0f4b
Joao Dias <filipevillar7@hotmail.com>
2019-12-05 11:01:14 +0000

    hub completo

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float horizontal, vertical;
+
+    [SerializeField]
+    private float runSpeed = 20.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
