247f584838f16da52f294ce281321e88a5ad5f40
Joao Dias <filipevillar7@hotmail.com>
2019-12-05 13:22:17 +0000

    stage2 changed with box and button

---
+++ Assets/Stage2Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2Button : MonoBehaviour
+{
+    bool pressedDown;
+
+    private void FixedUpdate()
+    {
+        Debug.Log(pressedDown);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!pressedDown && collision.CompareTag("Object"))
+        {
+            pressedDown = true;
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.2f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(pressedDown && collision.CompareTag("Object"))
+        {
+            pressedDown = false;
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 0.2f);
+        }
+    }
+}
